130fdd702863207940e3f91aa9dc38a24e7491b0
Clarence <c.mes@cm0.dev>
2023-05-26 23:10:22 +1000

    inital commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new Typed('#typed', {
+    strings: [
+        'An IT Technician', 
+        'A Full Stack Developer', 
+        'An Automation Specialist', 
+        'A (Future) System Administrator',
+        'A Computer Repair Technician',
+    ],
+    typeSpeed: 50,
+    backSpeed: 20,
+    loop: true,
+    smartBackspace: true,
+    backDelay: 3000,
+    shuffle: true,
+    cursorChar: '_',
+});
